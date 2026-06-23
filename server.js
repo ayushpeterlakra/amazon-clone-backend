7e0f3a202abbfb0a843651d9b60936384b3c462d
ayushpeterlakra <ayushlakra01@gmail.com>
2026-06-24 04:01:04 +0530

    Add authentication: signup and login routes with JWT and bcrypt

--- server.js
+++ server.js
@@ -1,27 +1,26 @@
 require("dotenv").config();
 
 const express = require("express");
-const Product = require("./models/Product");
 const mongoose = require("mongoose");
+const cors = require("cors");
+const productRoutes = require("./routes/productRoutes");
+const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
-console.log("MONGO_URI =", process.env.MONGO_URI);
+app.use(cors());
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
   .catch((err) => console.log("❌ Error:", err));
 
+app.use("/api/products", productRoutes);
+app.use("/api/auth", authRoutes);
+
 app.get("/", (req, res) => {
   res.send("Amazon Clone Backend Running");
-});
-
-app.get("/api/products", async (req, res) => {
-  console.log("Products route hit");
-
-  const products = await Product.find();
-  res.json(products);
 });
 
 app.listen(process.env.PORT, () => {
