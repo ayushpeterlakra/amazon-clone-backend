a721d0214af1470df74537b71e416fe668084794
ayushpeterlakra <ayushlakra01@gmail.com>
2026-06-24 22:27:50 +0530

    Add GTA 6 product and update seed.js

--- seed.js
+++ seed.js
@@ -9,17 +9,22 @@ const products = [
   {
     name: "iPhone 15",
     price: 79999,
-    image: "https://via.placeholder.com/200",
+    image: "/images/iphone15.png",
   },
   {
     name: "Samsung Galaxy S24",
     price: 69999,
-    image: "https://via.placeholder.com/200",
+    image: "/images/samsung_s24.png",
   },
   {
     name: "Sony Headphones",
     price: 9999,
-    image: "https://via.placeholder.com/200",
+    image: "/images/sony_headphones.png",
+  },
+  {
+    name: "GTA 6",
+    price: 4999,
+    image: "/images/gta6.png",
   },
 ];
 
